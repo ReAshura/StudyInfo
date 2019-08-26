47198cbc90bc17a8be8539079ff3ddb08a004def
ZhiqiangWang <43202146+ReAshura@users.noreply.github.com>
2019-08-26 18:16:50 +0800

    教师端主页面内容
    2019年8月26日18:16:29

---
+++ components/teacher-video/teacher-video.js
@@ -0,0 +1,30 @@
+// components/teacher-video/teacher-video.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    runAllList: {
+      type: Boolean,
+      value: false
+    },
+    titleShow: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
